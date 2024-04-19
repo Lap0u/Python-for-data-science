344553c69974716ba6352484ed618f6c663fa56c
cbeaurain <clementbeaurainpro@gmail.com>
2024-04-19 22:35:56 +0200

    feat(): 2.1 ok, 2.2 init

--- 2 - Datatable/ex01/aff_life.py
+++ 2 - Datatable/ex01/aff_life.py
@@ -1,11 +1,19 @@
 from load_csv import load
 import matplotlib.pyplot as plt
-import numpy as np
 
 
 def main():
     """Plots the distribution of life expectancy in France."""
     df = load("life_expectancy_years.csv")
+    france = df[df["country"] == "France"].iloc[0, 1:]
+    columns = df.columns[1:]
+    ticks = [x for x in columns if int(x) % 40 == 0]
+    plt.plot(france)
+    plt.xlabel("Year")
+    plt.ylabel("Life expectancy")
+    plt.title("France Life expectancy projections")
+    plt.xticks(ticks)
+    plt.show()
 
 
 if __name__ == "__main__":
