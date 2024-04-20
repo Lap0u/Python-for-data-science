658daa3f1ea23820523c368c92b4ef65760f076f
cbeaurain <clementbeaurainpro@gmail.com>
2024-04-21 00:40:39 +0200

    feat(): 2.2 ok

--- 2 - Datatable/ex02/aff_pop.py
+++ 2 - Datatable/ex02/aff_pop.py
@@ -2,26 +2,35 @@ from load_csv import load
 import matplotlib.pyplot as plt
 
 
+def convert_units(s):
+    """Converts val with units such as M or k or B to its litteral val"""
+    if s[-1] == "M":
+        return int(float(s[:-1]) * 10**6)
+    if s[-1] == "k":
+        return int(float(s[:-1]) * 10**3)
+    if s[-1] == "B":
+        return int(float(s[:-1]) * 10**9)
+    return int(s)
+
+
 def main():
     """Plots the population in France vs Belgium over the years."""
     df = load("population_total.csv")
-    france = df[df["country"] == "France"].iloc[0, 1:]
-    belgium = df[df["country"] == "Belgium"].iloc[0, 1:]
-    for i in range(len(france)):
-        france[i] = france[i][:-1]
-        belgium[i] = belgium[i][:-1]
-    print(france, france[1])
-    print(belgium)
-    columns = df.columns[1:]
-    ticks_x = [x for x in columns if int(x) % 40 == 0]
-    ticks_y = [20, 40, 60]
+    df.set_index("country", inplace=True)
+    for col in df.columns:
+        df[col] = df[col].apply(convert_units)
+    france = df.loc["France"]
+    belgium = df.loc["Belgium"]
+    ticks_x = [x for x in df.columns if int(x) % 40 == 0]
+    ticks_y = [20_000_000, 40_000_000, 60_000_000]
+    ticks_y_labels = ["20M", "40M", "60M"]
     plt.plot(belgium, label="Belgium")
     plt.plot(france, label="France")
     plt.xlabel("Year")
     plt.ylabel("Population")
     plt.title("Population projections")
     plt.xticks(ticks_x)
-    plt.yticks(ticks_y)
+    plt.yticks(ticks_y, ticks_y_labels)
     plt.legend()
     plt.xlim(["1800", "2050"])
     plt.show()
